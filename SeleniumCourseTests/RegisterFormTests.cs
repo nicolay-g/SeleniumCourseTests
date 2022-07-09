b31ab9b22668edc05bdcf2b275de05d6adb08f97
nicolay-g <ng82@mail.bg>
2022-07-09 11:04:35 +0300

    Clean RegisterFormTests class before creating a branch

--- SeleniumCourseTests/RegisterFormTests.cs
+++ SeleniumCourseTests/RegisterFormTests.cs
@@ -2,32 +2,13 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using SeleniumCourseTests;
 
 namespace CourseSeleniumTests
 {
     internal class RegisterFormTests : BaseTests
     {
-        protected override string Url => @"file:///C:/Learning/VisualStudioProjects/SeleniumTests-master/SeleniumTests/Htmls/Register/register.html";
+        protected override string Url => @"file:///C:/Learning/VisualStudioProjects/SeleniumCourseTests/SeleniumCourseTests/RegisterForm/register.html";
 
-        [Test]
-        public void GoogleSearchTest()
-        {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(@"https://www.google.com/");
-            driver.Manage().Window.Minimize();
-
-            IWebElement searchElement = driver.FindElement(By.Name("q"));
-            searchElement.SendKeys("Selenium Wikipedia");
-
-            IWebElement searchButton = driver.FindElement(By.Name("btnK"));
-            searchButton.Click();
-
-            IWebElement firstResult = driver.FindElement(By.CssSelector("h3"));
-            firstResult.Click();
-
-            Assert.That(firstResult, Is.EqualTo("Selenium - Wikipedia"));
-
-
-        }
     }
 }
